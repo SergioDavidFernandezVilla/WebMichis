58bd77f395cd043dd90b0cc0a203f0d38f0838bd
MrUserUnknown <gatocombotasfernandez@gmail.com>
2023-09-23 23:39:18 -0500

    Solucionado para el Samung Galaxy A12

--- src/pages/RazaGato.jsx
+++ src/pages/RazaGato.jsx
@@ -85,8 +85,6 @@ export default function RazaGato() {
               {/*PONER UN TAL VEZ UNA GALERIA XD*/}
             </section>
           </div>
-
-          <Footer></Footer>
         </div>
       </>
     );
